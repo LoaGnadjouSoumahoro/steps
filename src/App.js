e59c8c6b0a62b5b9e6975269488e2b28f23f7668
loa gnadjou soumahoro <loagnadjou@gmail.com>
2024-02-15 16:56:51 +0100

    Ajout des Evenst au bouttons Previous et Next

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 export default function App() {
+  const step = 1;
+  function handlePrevious() {
+    alert("Previous");
+  }
   return (
     <div className="steps">
       <div className="numbers">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+      <p className="message">
+        Steps {step}: {messages[step - 1]}
+      </p>
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
+          Previous
+        </button>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={() => alert("Next")}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
